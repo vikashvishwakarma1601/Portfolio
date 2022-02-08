ddb2fb9f34889873b7bd39037ef665bfc027013e
Vikash Vishwakarma <vishwakarma.vikash1601@gmail.com>
2022-02-09 00:54:30 +0530

    Add files via upload

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,24 +1,33 @@
 const bg1 = document.querySelector(".container-bg");
 const col_box = document.querySelector(".skills-bg");
-
-const windowWidth = bg1.style.width;
-const windowDimension = window.innerWidth;
+const certification = document.querySelector("#certification");
+const about = document.querySelector(".about_bg");
+const windowWidth = window.innerWidth;
 window.addEventListener("scroll", (event) => {
   const scrollY = window.scrollY;
   if (scrollY > 10 && scrollY < 450) {
     if (scrollY <= 440) {
       col_box.style.transform = `rotate(${20 - (scrollY / 100) * 4.6}deg)`;
     }
-    if (windowDimension < 600) {
+    if (windowWidth < 600) {
       col_box.style.top = `${5 - scrollY / 100}%`;
+      if (scrollY > 900 && scrollY <= 1150) {
+        col_box.style.transform = `rotate(0deg)`;
+        certification.style.transform = `translateX(${100 - scrollY / 9}%)`;
+      }
     } else {
-      col_box.style.top = `${35 - scrollY / 100}%`;
+      col_box.style.top = `${(scrollY / 100) * 5}%`;
     }
-
     bg1.style.width = `${50 + (scrollY / 8) * 0.9}%`;
   }
-  if (scrollY > 440) {
+  if (scrollY > 440 && scrollY <= 900) {
     col_box.style.transform = `rotate(0deg)`;
+    certification.style.transform = `translateX(${100 - scrollY / 9}%)`;
+  }
+  if (windowWidth < 600 && scrollY > 1400 && scrollY <= 2450) {
+    about.style.left = `${100 - scrollY / 50}%`;
+  } else if (scrollY > 1400 && scrollY <= 2000) {
+    about.style.left = `${100 - scrollY / 40}%`;
   }
 });
 
@@ -53,11 +62,21 @@ let isActive = false;
 burgerMenu.addEventListener("click", (event) => {
   if (isActive) {
     burgerMenu.classList.remove("active");
-    menu.style.transform = 'translateY(-500px)';
+    menu.style.transform = "translateY(-500px)";
     isActive = false;
   } else {
     burgerMenu.classList.add("active");
-    menu.style.transform = 'translateY(0px)';
+    menu.style.transform = "translateY(0px)";
     isActive = true;
   }
 });
+
+
+const hyperLink = Array.from(document.getElementsByTagName('a'));
+const audio = document.querySelector('#audio')
+console.log(audio)
+hyperLink.forEach((link)=>{
+  link.addEventListener('click',()=>{
+    audio.play()
+  })
+})
